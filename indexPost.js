c3aac9270c4bbcda103bd9e9034db166dfe13145
Ian Lopes <lopescastroneto@gmail.com>
2023-05-29 01:41:59 -0300

    commitando indexPost modificacao

--- indexPost.js
+++ indexPost.js
@@ -1,9 +1,9 @@
-const express = require('express')
+const express = require('express') //importando o modulo express
 
-const app = express()
+const app = express() //criando a aplicacao do servidor
 
-app.listen('3000')
+app.listen('3000') //ouvindo o navegador
 
-app.use(express.json())
+app.use(express.json()) //middlware
 
-app.route('/').post((req,res)=> console.log(req.body))
+app.route('/').post((req,res)=> console.log(req.body)) //rota que envia os dados via post
