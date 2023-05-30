99fd1afe6e4c378de6e34e77ea8b6121a3fb7bad
Ian Lopes <lopescastroneto@gmail.com>
2023-05-29 23:13:01 -0300

    subindo inclusao de comentarios no arquivo

--- indexGet.js
+++ indexGet.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const axios = require('axios')
+const axios = require('axios') //importando o axios
 
 const app = express()
 
@@ -7,8 +7,8 @@ app.listen('3000')
 
 app.route('/').get((req,res) => {
 
-    axios.get('https://api.github.com/users/jakeliny')
-    .then(result => res.send(result.data))
-    .catch(error => console.error(error))
+    axios.get('https://api.github.com/users/jakeliny') //fazendo a requisição atraves da funcao get do axios
+    .then(result => res.send(result.data)) // then para caso der certo a promessa
+    .catch(error => console.error(error)) // catch para caso a promessa der errado
 
 })
