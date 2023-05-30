1d5c9ce3088e60b2cad7c1f5fa82ea77186f3715
Ian Lopes <lopescastroneto@gmail.com>
2023-05-29 23:08:59 -0300

    subindo novas alteracoes

--- indexGet.js
+++ indexGet.js
@@ -1,7 +1,14 @@
 const express = require('express')
+const axios = require('axios')
 
 const app = express()
 
 app.listen('3000')
 
-app.route('/').get((req,res)=> res.send("Hello"))
+app.route('/').get((req,res) => {
+
+    axios.get('https://api.github.com/users/jakeliny')
+    .then(result => res.send(result.data))
+    .catch(error => console.error(error))
+
+})
